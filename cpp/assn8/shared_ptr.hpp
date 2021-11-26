b8690eb3f92e1a322eb585237b2a994c31fa2ff3
Sean Collings <seanrcollings@gmail.com>
2021-11-26 15:49:23 -0700

    shared_ptr working

--- cpp/assn8/shared_ptr.hpp
+++ cpp/assn8/shared_ptr.hpp
@@ -25,6 +25,17 @@ namespace usu
       private:
         T* m_ptr;
         uint64_t* m_refCount;
+
+        // Performs shared cleanup
+        void cleanup()
+        {
+            (*m_refCount)--;
+            if (*m_refCount == 0)
+            {
+                delete m_ptr;
+                delete m_refCount;
+            }
+        }
     };
 
     template <typename T>
@@ -48,21 +59,36 @@ namespace usu
     {
         m_ptr = obj.m_ptr;
         m_refCount = obj.m_refCount;
-        obj.m_ptr = nullptr;
-        obj.m_refCount = nullptr;
+        obj.m_ptr = obj.m_refCount = nullptr;
     }
 
     // Destructor
     template <typename T>
     shared_ptr<T>::~shared_ptr()
     {
-        (*m_refCount)--;
+        cleanup();
+    }
 
-        if (*m_refCount == 0)
-        {
-            delete m_ptr;
-            delete m_refCount;
-        }
+    // Copy Assignment
+    template <typename T>
+    shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr<T>& obj)
+    {
+        cleanup();
+        m_ptr = obj.m_ptr;
+        m_refCount = obj.m_refCount;
+        (*m_refCount)++;
+        return *this;
+    }
+
+    // Move Assignment
+    template <typename T>
+    shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr<T>&& obj)
+    {
+        cleanup();
+        m_ptr = obj.m_ptr;
+        m_refCount = obj.m_refCount;
+        obj.m_ptr = obj.m_refCount = nullptr;
+        return *this;
     }
 
     template <typename T>
@@ -75,25 +101,6 @@ namespace usu
     uint64_t shared_ptr<T>::use_count()
     {
         return *m_refCount;
-    }
-
-    // Copy Assignment
-    template <typename T>
-    shared_ptr<T>& shared_ptr<T>::operator=(const shared_ptr<T>& obj)
-    {
-        m_ptr = obj.m_ptr;
-        m_refCount = obj.m_refCount;
-        (*m_refCount)++;
-        return *this;
-    }
-
-    // Move Assignment
-    template <typename T>
-    shared_ptr<T>& shared_ptr<T>::operator=(shared_ptr<T>&& obj)
-    {
-        m_ptr = obj.m_ptr;
-        m_refCount = obj.m_refCount;
-        return *this;
     }
 
     template <typename T>
@@ -120,123 +127,3 @@ namespace usu
     //     return shared_ptr<T[]>(new T[N], N);
     // }
 } // namespace usu
-
-// #include <cstdint>
-// #include <iostream>
-// #include <utility>
-
-// namespace usu
-// {
-
-//     template <class T>
-//     class shared_ptr
-//     {
-//       private:
-//         T* ptr = nullptr;
-//         uint64_t* refCount = nullptr;
-
-//       public:
-//         shared_ptr() :
-//             ptr(nullptr), refCount(new uint64_t(0)) // default constructor
-//         {
-//         }
-
-//         shared_ptr(T* ptr) :
-//             ptr(ptr), refCount(new uint64_t(1)) // constructor
-//         {
-//         }
-
-//         /*** Copy Semantics ***/
-//         shared_ptr(const shared_ptr& obj) // copy constructor
-//         {
-//             this->ptr = obj.ptr; // share the underlying pointer
-//             this->refCount = obj.refCount;
-//             if (nullptr != obj.ptr)
-//             {
-//                 (*this->refCount)++; // if the pointer is not null, increment the refCount
-//             }
-//         }
-
-//         shared_ptr& operator=(const shared_ptr& obj) // copy assignment
-//         {
-//             __cleanup__(); // cleanup any existing data
-
-//             // Assign incoming object's data to this object
-//             this->ptr = obj.ptr; // share the underlying pointer
-//             this->refCount = obj.refCount;
-//             if (nullptr != obj.ptr)
-//             {
-//                 (*this->refCount)++; // if the pointer is not null, increment the refCount
-//             }
-//             return *this;
-//         }
-
-//         /*** Move Semantics ***/
-//         shared_ptr(shared_ptr&& dyingObj) // move constructor
-//         {
-//             this->ptr = dyingObj.ptr; // share the underlying pointer
-//             this->refCount = dyingObj.refCount;
-
-//             dyingObj.ptr = dyingObj.refCount = nullptr; // clean the dying object
-//         }
-
-//         shared_ptr& operator=(shared_ptr&& dyingObj) // move assignment
-//         {
-//             __cleanup__(); // cleanup any existing data
-
-//             this->ptr = dyingObj.ptr; // share the underlying pointer
-//             this->refCount = dyingObj.refCount;
-
-//             dyingObj.ptr = dyingObj.refCount = nullptr; // clean the dying object
-//         }
-
-//         uint64_t use_count() const
-//         {
-//             return *refCount; // *this->refCount
-//         }
-
-//         T* get() const
-//         {
-//             return this->ptr;
-//         }
-
-//         T* operator->() const
-//         {
-//             return this->ptr;
-//         }
-
-//         T& operator*() const
-//         {
-//             return *ptr;
-//         }
-
-//         ~shared_ptr() // destructor
-//         {
-//             __cleanup__();
-//         }
-
-//       private:
-//         void __cleanup__()
-//         {
-//             (*refCount)--;
-//             if (*refCount == 0)
-//             {
-//                 if (nullptr != ptr)
-//                     delete ptr;
-//                 delete refCount;
-//             }
-//         }
-//     };
-
-//     template <typename T, typename... Args>
-//     shared_ptr<T> make_shared(Args&&... args)
-//     {
-//         return shared_ptr<T>(new T(std::forward<Args>(args)...));
-//     }
-
-//     // template <typename T, unsigned int N>
-//     // shared_ptr<T[]> make_shared_array()
-//     // {
-//     //     return shared_ptr<T[]>(new T[N], N);
-//     // }
-// } // namespace usu
