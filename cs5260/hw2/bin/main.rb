0fa253c702c48bd1ed2cc1c5c236221569a71f30
Sean Collings <seanrcollings@gmail.com>
2022-10-23 23:37:56 -0600

    fixed naming in main.rb

--- cs5260/hw2/bin/main.rb
+++ cs5260/hw2/bin/main.rb
@@ -4,31 +4,29 @@ require 'logger'
 
 require_relative "../src/consumer"
 
-module Consumer
-  class CLI < Thor
-    desc "consumer", 'execute the consumer program'
-    option :sbucket, desc: "The source S3 bucket name"
-    option :dservice, desc: "The service of the destination, either s3 or dynamoddb"
-    option :destination, desc: "The name of the destination bucket or table"
+class CLI < Thor
+  desc "consumer", 'execute the consumer program'
+  option :sbucket, desc: "The source S3 bucket name"
+  option :dservice, desc: "The service of the destination, either s3 or dynamoddb"
+  option :destination, desc: "The name of the destination bucket or table"
 
-    def consumer
-      logger = Logger.new(STDOUT)
-      case options[:dservice]
-      when 's3'
-        processor = Consumer::S3Processor.new(options[:destination], logger)
-      when 'dynamodb'
-        processor = Consumer::DynamoDBProcessor.new(options[:destination], logger)
-      else
-        raise 'Bad option for dservice'
-      end
-
-
-      retriever = Consumer::S3Retriever.new(options[:sbucket], logger)
-
-      poller = Consumer::Poller.new(retriever, processor, logger)
-      poller.execute
+  def consumer
+    logger = Logger.new(STDOUT)
+    case options[:dservice]
+    when 's3'
+      processor = Consumer::S3Processor.new(options[:destination], logger)
+    when 'dynamodb'
+      processor = Consumer::DynamoDBProcessor.new(options[:destination], logger)
+    else
+      raise 'Bad option for dservice'
     end
+
+
+    retriever = Consumer::S3Retriever.new(options[:sbucket], logger)
+
+    poller = Consumer::Poller.new(retriever, processor, logger)
+    poller.execute
   end
 end
 
-Consumer::CLI.start(ARGV)
+CLI.start(ARGV)
