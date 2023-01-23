89dac94a85093cce549b45a8a45815c2a10cca6f
Sean Collings <seanrcollings@gmail.com>
2023-01-23 10:40:44 -0700

    updated with new requirements

--- cs5060/hw1/README.md
+++ cs5060/hw1/README.md
@@ -7,6 +7,8 @@ Run with:
 $ python <FILENAME> p1
 ```
 
+**NOTE**: For the provided scenarios to run, the files need to be in the working directory, however it will not fail if they do not exist.
+
 ## Part 2
 - Uniform: 3-5%
 - Normal: 2-6%
--- cs5060/hw1/main.py
+++ cs5060/hw1/main.py
@@ -1,18 +1,26 @@
+import os
 from statistics import NormalDist
 import sys
 import random
+
+
+def from_file(filename):
+    with open(filename, "r") as f:
+        contents = f.readlines()
+
+    return [int(line) for line in contents]
 
 
 def uniform(length, start, end):
     return [random.uniform(start, end) for _ in range(length)]
 
 
-def part1(iterations: int, length: int):
+def part1(iterations, length):
     print(f"Performing {iterations} iterations, each with length {length}")
     percentages = {0.01 * i: 0 for i in range(1, 100)}
 
     for _ in range(iterations):
-        candidates: list[float] = uniform(length, 0, 100)
+        candidates = uniform(length, 0, 100)
         largest = max(candidates)
 
         for percent in percentages:
@@ -25,16 +33,59 @@ def part1(iterations: int, length: int):
                         percentages[percent] += 1
                     break
 
-    max_optimal = max(percentages.items(), key=lambda v: v[1])[0]
-    print(f"Optimal is about: {max_optimal * 100:.2f}%")
+    max_optimal_perc = max(percentages.items(), key=lambda v: v[1])[0]
+    print(f"Optimal is about: {max_optimal_perc * 100:.2f}%")
+
+    print("\n---Provided Scenarios----")
+    for file in ("scenario1.csv", "scenario2.csv"):
+        if not os.path.exists(file):
+            print(f"{file} does not etxist, skipping...")
+            continue
+
+        candidates = from_file(file)
+        file_optimal = find_optimal_solution(candidates)
+        file_perc = file_optimal / len(candidates)
+        print(f"Optimal stopping for {file}: {file_perc * 100:.2f}%")
+        print(
+            f"Largest using file optimal: {find_largest_after(candidates, round(len(candidates) * file_optimal))}"
+        )
+        print(
+            f"Largest using derived optimal (~37%): {find_largest_after(candidates, round(len(candidates) * max_optimal_perc))}"
+        )
+        print("")
 
 
-def part2(iterations: int, length: int, sample) -> None:
+def find_optimal_solution(candidates):
+    largest = max(candidates)
+
+    for i in range(1, len(candidates)):
+        max_before = max(candidates[:i])
+
+        for candiate in candidates[i:]:
+            if candiate > max_before:
+                if candiate == largest:
+                    return i
+                break
+
+    return None
+
+
+def find_largest_after(lst, index):
+    max_before = max(lst[:index])
+
+    for i in lst[1:]:
+        if i >= max_before:
+            return i
+
+    return None
+
+
+def part2(iterations, length, sample):
     print(f"Performing {iterations} iterations, each with length {length}")
     percentages = {0.01 * i: 0 for i in range(1, 100)}
 
     for _ in range(iterations):
-        candidates: list[float] = sample()
+        candidates = sample()
         largest = max(candidates)
 
         for percent in percentages:
@@ -55,7 +106,7 @@ def part2(iterations: int, length: int, sample) -> None:
 def main() -> None:
     if sys.argv[-1] == "p1":
         print("executing the code for part 1")
-        part1(10000, 1000)
+        part1(10000, 100)
     elif sys.argv[-1] == "p2":
         iters = 1000
         length = 100
